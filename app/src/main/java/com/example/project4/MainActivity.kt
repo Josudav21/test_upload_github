1ebd9dd9d810e57df64263bb64a84daa41adc0a5
Josua Guntur <josuaguntur@gmail.com>
2022-02-15 18:12:46 +0700

    first project initialization

---
+++ app/src/main/java/com/example/project4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.project4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}git remote add origin https://github.com/Josudav21/test_upload_github.git
